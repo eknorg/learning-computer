5153b74aecefc40306a782bb3dab3a668fd297f2
eknorg <gronke@gmail.com>
2016-07-05 12:32:09 -0400

    Add files via upload

--- learningcomputer.py
+++ learningcomputer.py
@@ -2,24 +2,38 @@
 
 
 #Reads text file
-lines = []
-source = open('tweets','r')
-for line in source:
-    lines.append(line)
-source.close()
+#lines = []
+#source = open('tweets','r')
+#for line in source:
+#    lines.append(line)
+#source.close()
 
 codex = {}
 
-linetest = []
+#linetest = []
 
-for r in lines[:5]
-    row = r.split()[:-3]
-    linetest.append(row)
+testline = 'the cat in the hat'.split()
 
-for n in range len(linetest):
-    for i in range(len(linetest[n])):
+#for r in lines[:5]
+#    row = r.split()[:-3]
+#    linetest.append(row)
+
+#for n in range len(linetest):
+#    for i in range(len(linetest[n])):
+#        if i in codex:
+#            #If in dictionary, check to see if word is in key    
+#        else: 
+#            #If not in dictionary, add it with next word
+#            codex[i] = i+1            
+
+for i in range(len(testline[n])):
         if i in codex:
-            #If in dictionary, check to see if word is in key    
-        else: 
-            #If not in dictionary, add it with next word
-            codex[i] = i+1            
+            for listing in codex[i]:
+                
+                
+        else:
+            codex[i] = i+1
+            
+            
+            
+            
